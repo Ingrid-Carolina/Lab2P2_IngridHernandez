3ee55b99cf6ef0e0e19b1c238952712a8808bad7
Ingrid Hernandez <ingrid_Hernandez@unitec.edu>
2022-10-21 13:49:48 -0600

    avance 4

---
+++ src/lab2p2_ingridhernandez_12141186/Tablero.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab2p2_ingridhernandez_12141186;
+
+/**
+ *
+ * @author ADMINISTRADOR1
+ */
+public class Tablero {
+    char[][] tablero = new char[30][30];
+
+    public Tablero() {
+        imprimirTablero(0, 0);
+    }
+  
+    
+    
+    public void imprimirTablero(int filas, int columnas) {
+        if (filas == tablero.length - 1 && columnas == tablero[0].length - 1) {
+            System.out.printf("[%c]%n", tablero[filas][columnas]);
+        } else {
+            //Salto de Linea
+            if (columnas == tablero[0].length - 1) {
+                System.out.printf("[%c]%n", tablero[filas][columnas]);
+                imprimirTablero(filas + 1, 0);
+            } else {
+                System.out.printf("[%c] ", tablero[filas][columnas]);
+                imprimirTablero(filas, columnas + 1);
+            }//fin if
+        }//fin else
+    }//fin metodo
+  
+}
