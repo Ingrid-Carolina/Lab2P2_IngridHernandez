4c21feb93e0d464815ebe2704e6f6e9c993c0668
Ingrid Hernandez <ingrid_Hernandez@unitec.edu>
2022-10-21 13:32:56 -0600

    avance 1

--- src/lab2p2_ingridhernandez_12141186/Lab2P2_IngridHernandez_12141186.java
+++ src/lab2p2_ingridhernandez_12141186/Lab2P2_IngridHernandez_12141186.java
@@ -4,6 +4,8 @@
  */
 package lab2p2_ingridhernandez_12141186;
 
+import java.util.Scanner;
+
 /**
  *
  * @author ADMINISTRADOR1
@@ -15,6 +17,40 @@ public class Lab2P2_IngridHernandez_12141186 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner lea = new Scanner(System.in);
+        
+        boolean centinela = true;
+        
+        while(centinela == true){
+            System.out.println("==========Menu=========");
+            System.out.println("0)Salir");
+            System.out.println("1)Agregar Jugador");
+            System.out.println("2)Bot");
+            System.out.println("3)Agregar Item a Juagdor" );
+            System.out.println("4)Jugar");
+             System.out.print("Ingrese la opcion que desea: ");
+            int opcion = lea.nextInt();
+            switch (opcion){
+                case 1:{
+                    
+                }//fin del case 1
+                break;
+                case 2:{
+                    
+                }//fin del case 2
+                break;
+                case 3:{
+                    
+                }//fin del case 3
+                break;
+                case 0:
+                    centinela = false;
+                 break;
+           
+                default:
+           System.out.println("Opcion Incorrecta");
+            }//fin del switch
+    }//fin while
     }
     
 }
